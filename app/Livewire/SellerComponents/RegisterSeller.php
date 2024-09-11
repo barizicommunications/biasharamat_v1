3858e9634f8418240f13038c057534ad77aa5f54
Hardy-B961 <kathurimahardy233@gmail.com>
2024-09-11 16:55:52 +0300

    added components

--- app/Livewire/SellerComponents/RegisterSeller.php
+++ app/Livewire/SellerComponents/RegisterSeller.php
@@ -4,9 +4,13 @@ namespace App\Livewire\SellerComponents;
 
 use Livewire\Component;
 use Filament\Forms\Form;
+use Filament\Forms\Components\Select;
 use Filament\Forms\Components\Wizard;
 use Filament\Forms\Contracts\HasForms;
+use Filament\Forms\Components\Checkbox;
+use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Components\RichEditor;
 use Filament\Forms\Concerns\InteractsWithForms;
 
@@ -21,28 +25,87 @@ class RegisterSeller extends Component implements HasForms
         ->schema([
             Wizard::make([
                 Wizard\Step::make('Client Information')
-                ->description('The information provided will remain confidential and will not be publicly displayed.')
+                ->description('Client info')
                     ->schema([
 
                         TextInput::make('name')
-                        ->required(),
-                        TextInput::make('company_name')
-                        ->required(),
+                        ->required()
+                        ->label('Name'),
+                    TextInput::make('company_name')
+                        ->required()
+                        ->label('Company Name'),
+                    TextInput::make('mobile_number')
+                        ->required()
+                        ->label('Mobile Number'),
+                    TextInput::make('email')
+                        ->required()
+                        ->label('Email'),
+                        Checkbox::make('display_company_details')
+                        ->label('Display company details to introduced members so that they can know about my company')
+                        ->columnSpan('full')
 
                         // ...
                     ])->columns(2),
-                Wizard\Step::make('Delivery')
+                Wizard\Step::make('Business Information')
                     ->schema([
+
+                        Select::make('seller_role')
+                            ->options([
+                                'Director' => 'Director',
+                                'Adviser' => 'Adviser',
+                                'Shareholder' => 'Shareholder',
+                                'Other' => 'Other',
+                            ])
+                            ->required()
+                            ->label('Seller Role'),
+                        TextInput::make('seller_interest')
+                            ->required()
+                            ->label('Seller Interest'),
+                        DatePicker::make('business_start_date')
+                            ->required()
+                            ->label('Business Start Date'),
+                        TextInput::make('business_industry')
+                            ->required()
+                            ->label('Business Industry'),
+                        TextInput::make('country')
+                            ->required()
+                            ->label('Country'),
+                        TextInput::make('city')
+                            ->required()
+                            ->label('City'),
+                        TextInput::make('county')
+                            ->required()
+                            ->label('County'),
+                        TextInput::make('number_employees')
+                            ->required()
+                            ->label('Number of Employees'),
+                        TextInput::make('business_legal_entity')
+                            ->required()
+                            ->label('Business Legal Entity'),
+                        TextInput::make('website_link')
+                            ->label('Website Link'),
+                            Textarea::make('business_description')
+                            ->required()
+                            ->label('Business Description'),
+                        Textarea::make('product_services')
+                            ->required()
+                            ->label('Product/Services'),
+                        Textarea::make('business_highlights')
+                            ->label('Business Highlights'),
+                        Textarea::make('facility_description')
+                            ->label('Facility Description'),
+
+
                         // ...
                     ]),
                 Wizard\Step::make('Billing')
                     ->schema([
                         // ...
                     ]),
-            ])
+            ])->skippable()
         ]);
 }
-    
+
     public function render()
     {
         return view('livewire.seller-components.register-seller');
--- resources/views/livewire/seller-components/register-seller.blade.php
+++ resources/views/livewire/seller-components/register-seller.blade.php
@@ -1,7 +1,9 @@
 <div>
-    <h1 class="text-8xl">This is the business seller registration</h1>
 
-    <form action="" class="px-8">
-        {{ $this->form }}
-    </form>
+
+    <div class="max-w-screen-xl mx-auto py-8">
+        <form action="" class="px-8">
+            {{ $this->form }}
+        </form>
+    </div>
 </div>
